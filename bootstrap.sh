fbbde55b35d3336c2c993bd23a0bdea50a677126
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2013-11-06 11:06:53 +0100

    Reordered provision

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,31 +4,32 @@
 # =======
 
 # Basic system requirements
+# -------------------------
 sudo apt-get update
-sudo apt-get install -y curl bison git autoconf zlib1g-dev libbz2-dev libreadline6 libreadline6-dev libxslt1-dev g++ libyaml-0-2 libyaml-dev openssl make
+sudo apt-get install -y \
+  bison autoconf g++ libxslt1-dev make \
+  zlib1g-dev libbz2-dev libreadline6 libreadline6-dev \
+  curl git openssl libyaml-0-2 libyaml-dev \
+  ruby1.9.1 ruby1.9.1-dev \
+  r-base-core r-base-dev \
+  openjdk-7-jdk \
+  libtokyocabinet-dev tokyocabinet-bin
 
-# Ruby
-sudo apt-get install -y ruby1.9.1 ruby1.9.1-dev
+# Ruby gems and Rbbt
+# -------------------------
 
 # R (extra config in gem)
-sudo apt-get install -y r-base-core r-base-dev
 sudo gem install --no-ri --no-rdoc rsruby -- --with-R-dir=/usr/lib/R --with-R-include=/usr/share/R/include 
 
 # Java (extra config in gem)
-sudo apt-get install -y openjdk-7-jdk 
 sudo env JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386 gem install --no-ri --no-rdoc rjb
 
-# Rbbt
-sudo gem install --no-ri --no-rdoc rbbt-util rbbt-rest rbbt-study rbbt-dm rbbt-text rbbt-sources rbbt-phgx
-
-# Extra gems (reason they are not installed by default)
-    # JS compression (slow to compile)
-sudo gem install --no-ri --no-rdoc uglifier therubyracer
-    # Tokyocabinet (problematic sometimes)
-sudo apt-get install -y libtokyocabinet-dev tokyocabinet-bin
-sudo gem install --no-ri --no-rdoc tokyocabinet
-    # Ruby-prof (problematic sometimes)
-sudo gem install --no-ri --no-rdoc ruby-prof
+# Rbbt and some optional gems
+sudo gem install --no-ri --no-rdoc \
+    rbbt-util rbbt-rest rbbt-study rbbt-dm rbbt-text rbbt-sources rbbt-phgx \
+    tokyocabinet \
+    uglifier therubyracer \
+    ruby-prof
 
 # CONFIG
 # ======

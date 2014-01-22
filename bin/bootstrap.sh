19a6092442f344c717e6bfc01fa88419d9d6ab76
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2014-01-22 17:44:43 +0100

    Fixed a bug in the R_HOME variable export and some more logs

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -11,5 +11,6 @@ export RBBT_WORKFLOW_AUTOINSTALL
 export RBBT_LOG
 
 for workflow in $BOOTSTRAP_WORKFLOWS; do
+    echo "Bootstrapping $workflow on $BOOTSTRAP_CPUS CPUs"
     rbbt workflow cmd $workflow bootstrap $BOOTSTRAP_CPUS
 done

946c09af36b3e3190aa29df6f9496b5da3df503d
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2015-10-28 11:53:34 +0100

    nil

--- bin/run_rbbt_docker.rb
+++ bin/run_rbbt_docker.rb
@@ -9,14 +9,16 @@ options = SOPT.setup <<EOF
 
 Runs a docker image from an infrastructure definition file
 
-$ #{ $0 } [options] <infrastructure.yaml>
+$ #{ $0 } [options] <infrastructure.yaml> <command> <args>
 
 Infrastruture definition comes in YAML
 
 -h--help Print this help
 
 EOF
-infrastructure_file = ARGV.shift
+infrastructure_file, cmd, *cmd_args = ARGV
+
+cmd_args.collect!{|a| "'" << a << "'" }
 
 if options[:help] or infrastructure_file.nil?
   if defined? rbbt_usage
@@ -28,7 +30,39 @@ if options[:help] or infrastructure_file.nil?
 end
 
 
-infrastructure = YAML.load infrastructure_file
+infrastructure = File.open(infrastructure_file){|io| YAML.load io }
+IndiferentHash.setup(infrastructure)
 
-iii infrastructure
+image = infrastructure[:image]
+
+if user = infrastructure[:user]
+  user_conf = "-u #{user} -e HOME=/home/#{user}/ "
+else
+  user_conf = ""
+end
+
+mount_conf = ""
+if infrastructure[:mounts]
+  infrastructure[:mounts].each do |target,source|
+    target = target.gsub("USER", user)
+    if source.nil? or source.empty?
+      mount_conf << " -v #{target}"
+    else
+      mount_conf << " -v #{File.expand_path(source)}:#{target}"
+    end
+  end
+end
+
+if infrastructure[:workflow_autoinstall] and infrastructure[:workflow_autoinstall].to_s == 'true'
+  cmd = "env RBBT_WORKFLOW_AUTOINSTALL=true " + cmd
+end
+
+cmd_str = "docker run #{mount_conf} #{user_conf} #{image} #{cmd} #{cmd_args*" "} "
+Log.info "Running docker: \n" << cmd_str
+Log.severity = 0
+io = CMD.cmd(cmd_str, :pipe => true, :log => true, :stderr => 0)
+
+while line = io.gets
+  puts line
+end
 

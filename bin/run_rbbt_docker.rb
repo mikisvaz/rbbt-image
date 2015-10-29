ad31641883c33dee6b2bfda7fae875df4eb14b25
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2015-10-29 18:33:41 +0100

    nil

--- bin/run_rbbt_docker.rb
+++ bin/run_rbbt_docker.rb
@@ -9,16 +9,26 @@ options = SOPT.setup <<EOF
 
 Runs a docker image from an infrastructure definition file
 
-$ #{ $0 } [options] <infrastructure.yaml> <command> <args>
+$ #{ $0 } [options] <infrastructure.yaml> <command> <args> -- <extra docker options>
 
 Infrastruture definition comes in YAML
 
 -h--help Print this help
+--log* Log level
 
 EOF
-infrastructure_file, cmd, *cmd_args = ARGV
 
-cmd_args.collect!{|a| "'" << a << "'" }
+Log.severity = options[:log].to_i if options[:log]
+
+module Log
+  def self.log(msg,level)
+    msg = "" if msg.nil?
+    puts msg
+  end
+end
+
+infrastructure_file, cmd, *args = ARGV
+
 
 if options[:help] or infrastructure_file.nil?
   if defined? rbbt_usage
@@ -29,6 +39,15 @@ if options[:help] or infrastructure_file.nil?
   exit 0
 end
 
+cmd_args = []
+while args[0] != '--'
+  cmd_args << args.shift
+end
+
+docker_args = args[1..-1]
+
+cmd_args.collect!{|a| '"' << a << '"' }
+docker_args.collect!{|a| '"' << a << '"' }
 
 infrastructure = File.open(infrastructure_file){|io| YAML.load io }
 IndiferentHash.setup(infrastructure)
@@ -48,18 +67,22 @@ if infrastructure[:mounts]
     if source.nil? or source.empty?
       mount_conf << " -v #{target}"
     else
+      FileUtils.mkdir_p source unless File.directory? source
+      FileUtils.chmod 0777, source
       mount_conf << " -v #{File.expand_path(source)}:#{target}"
     end
   end
 end
 
-if infrastructure[:workflow_autoinstall] and infrastructure[:workflow_autoinstall].to_s == 'true'
+if infrastructure[:workflow_autoinstall] and infrastructure[:workflow_autoinstall].to_s == 'true' and cmd =~ /rbbt/
   cmd = "env RBBT_WORKFLOW_AUTOINSTALL=true " + cmd
 end
 
-cmd_str = "docker run #{mount_conf} #{user_conf} #{image} #{cmd} #{cmd_args*" "} "
-Log.info "Running docker: \n" << cmd_str
-Log.severity = 0
+cmd_str = "docker run #{mount_conf} #{user_conf} #{docker_args*" "} #{image} /bin/bash --login -c '#{cmd} #{cmd_args*" "}"
+cmd_str += " --log #{Log.severity} " if cmd =~  / rbbt$/
+cmd_str += "'" 
+
+Log.info "Docker: \n" << cmd_str
 io = CMD.cmd(cmd_str, :pipe => true, :log => true, :stderr => 0)
 
 while line = io.gets

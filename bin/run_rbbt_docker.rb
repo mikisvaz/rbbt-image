d01ababdc481b3d0c6eb25a8b70fda0a72521f75
Miguel Vazquez <miguel.vazquez@cnio.es>
2016-09-06 17:49:02 +0200

    nil

--- bin/run_rbbt_docker.rb
+++ bin/run_rbbt_docker.rb
@@ -2,6 +2,7 @@
 
 require 'rbbt-util'
 require 'rbbt/util/simpleopt'
+require 'rbbt/docker'
 
 $0 = "rbbt #{$previous_commands*""} #{ File.basename(__FILE__) }" if $previous_commands
 
@@ -15,7 +16,8 @@ Infrastruture definition comes in YAML.
 
 -h--help Print this help
 --log* Log level
-
+-d--dry_run Dry run
+-n--name* Container name
 EOF
 
 
@@ -53,59 +55,78 @@ end
 docker_args = args[1..-1] || []
 
 cmd_args.collect!{|a| '"' << a << '"' }
-docker_args.collect!{|a| '"' << a << '"' }
 
+infrastructure_file = Rbbt.etc.infrastructure[infrastructure_file+'.yaml'].find unless File.exists? infrastructure_file
 infrastructure = File.open(infrastructure_file){|io| YAML.load io }
-IndiferentHash.setup(infrastructure)
 
-image = infrastructure[:image]
+RbbtDocker.load_infrastructure(infrastructure, cmd, cmd_args, docker_args, options)
 
-if user = infrastructure[:user]
-  user_conf = "-u #{user} -e HOME=/home/#{user}/ -e USER=#{user}"
-  user_conf = "-e HOME=/home/#{user}/ -e USER=#{user}"
-else
-  user_conf = ""
-end
-
-mount_conf = ""
-if infrastructure[:mounts]
-  infrastructure[:mounts].each do |target,source|
-    target = target.gsub("USER", user) if target.include? "USER"
-    if source.nil? or source.empty?
-      mount_conf << " -v #{target}"
-    else
-      if not File.directory? source
-        FileUtils.mkdir_p source 
-        FileUtils.chmod 0777, source
-      end
-      mount_conf << " -v #{File.expand_path(source)}:#{target}"
-    end
-  end
-end
-
-if infrastructure[:workflow_autoinstall] and infrastructure[:workflow_autoinstall].to_s == 'true' and cmd =~ /rbbt/
-  cmd = "env RBBT_WORKFLOW_AUTOINSTALL=true " + cmd
-end
-
-umask = infrastructure[:umask] ? 'umask 000; ' : ''
-cmd_str = "docker run #{mount_conf} #{user_conf} #{docker_args*" "} #{image} /bin/bash --login -c '#{umask}#{cmd} #{cmd_args*" "}"
-cmd_str += " --log #{Log.severity} " if cmd =~  /\brbbt$/
-cmd_str += "'" 
-
-Log.info "Docker: \n" << cmd_str
-exec(cmd_str)
+#docker_args.collect!{|a| '"' << a << '"' }
+#
+#infrastructure = File.open(infrastructure_file){|io| YAML.load io }
+#IndiferentHash.setup(infrastructure)
+#
+#image = infrastructure[:image]
+#
+#if user = infrastructure[:user]
+#  user_conf = "-u #{user} -e HOME=/home/#{user}/ -e USER=#{user}"
+#  user_conf = "-e HOME=/home/#{user}/ -e USER=#{user}"
+#else
+#  user_conf = ""
+#end
+#
+#mount_conf = ""
+#seen_mounts = {}
+#if infrastructure[:mounts]
+#  infrastructure[:mounts].each do |target,source|
+#    target = target.gsub("USER", user) if target.include? "USER"
+#    if source.nil? or source.empty?
+#      mount_conf << " -v #{target}"
+#    else
+#      matches = seen_mounts.select{|starget,ssource| Misc.path_relative_to starget, target }
+#
+#      if matches.any?
+#        matches.each do |starget,ssource|
+#          subdir = Misc.path_relative_to starget, target
+#          dir = File.join(ssource, File.dirname(subdir))
+#          if not File.directory? dir
+#            FileUtils.mkdir_p dir 
+#            FileUtils.chmod 0777, dir
+#          end
+#        end
+#
+#      end
+#
+#      if not File.directory? source
+#        FileUtils.mkdir_p source 
+#        FileUtils.chmod 0777, source
+#      end
+#      seen_mounts[target] = source
+#      mount_conf << " -v #{File.expand_path(source)}:#{target}"
+#    end
+#  end
+#end
+#
+#if infrastructure[:workflow_autoinstall] and infrastructure[:workflow_autoinstall].to_s == 'true' and cmd =~ /rbbt/
+#  cmd = "env RBBT_WORKFLOW_AUTOINSTALL=true " + cmd
+#end
+#
+#umask = infrastructure[:umask] ? 'umask 000; ' : ''
+#cmd_str = "docker run #{mount_conf} #{user_conf} #{docker_args*" "} #{image} /bin/bash --login -c '#{umask}#{cmd} #{cmd_args*" "}"
+#cmd_str += " --log #{Log.severity} " if cmd =~  /\brbbt$/
+#cmd_str += "'" 
+#
+#Log.info "Docker: \n" << cmd_str
+#exec(cmd_str) unless options[:dry_run]
 
 __END__
----
-:image: mikisvaz/rbbt-basic
-:user: rbbt
-:umask: true
-:mounts:
-    /usr/local/share/rbbt/organisms: /data2/rbbt/share/organisms
-    /usr/local/share/rbbt/data/studies: /data2/rbbt/share/data/projects/ICGC
-    /usr/local/workflows/rbbt: /home/mvazquezg/git/workflows
-    /usr/local/apps/rbbt: /home/mvazquezg/git/apps
-    /home/USER/.rbbt/var: ./var
-    /home/USER/.rbbt/etc: ./etc
-    /home/USER/.rbbt/var/jobs/Sample: ./Sample
-    /home/USER/.rbbt/var/jobs/Study: ./Study
+image: mikisvaz/rbbt-basic
+user: rbbt
+umask: true
+workflow_autoinstall: true
+mounts:
+    /home/USER/.rbbt/: ./.rbbt
+    /home/USER/.rbbt/share/databases: /data3/rbbt/share/databases
+    /home/USER/.rbbt/share/organisms: /data3/rbbt/share/organisms
+    /home/USER/.rbbt/var/dbNSFP: /data3/rbbt/var/dbNSFP
+    /home/USER/.rbbt/var/DbSNP: /data3/rbbt/var/DbSNP
---
+++ lib/rbbt/docker.rb
@@ -0,0 +1,66 @@
+module RbbtDocker
+  def self.load_infrastructure(infrastructure, cmd, cmd_args = [], docker_args = [], options = {})
+    cmd_args.collect!{|a| '"' << a << '"' }
+    docker_args.collect!{|a| '"' << a << '"' }
+
+
+    IndiferentHash.setup(infrastructure)
+
+    image = infrastructure[:image]
+
+    if user = infrastructure[:user]
+      user_conf = "-u #{user} -e HOME=/home/#{user}/ -e USER=#{user}"
+      user_conf = "-e HOME=/home/#{user}/ -e USER=#{user}"
+    else
+      user_conf = ""
+    end
+
+    mount_conf = ""
+    seen_mounts = {}
+    if infrastructure[:mounts]
+      infrastructure[:mounts].each do |target,source|
+        target = target.gsub("USER", user) if target.include? "USER"
+        if source.nil? or source.empty?
+          mount_conf << " --volumes-from #{target}"
+        else
+          matches = seen_mounts.select{|starget,ssource| Misc.path_relative_to starget, target }
+
+          if matches.any?
+            matches.each do |starget,ssource|
+              subdir = Misc.path_relative_to starget, target
+              dir = File.join(ssource, File.dirname(subdir))
+              if not File.directory? dir
+                FileUtils.mkdir_p dir 
+                FileUtils.chmod 0777, dir
+              end
+            end
+
+          end
+
+          if not File.directory? source
+            FileUtils.mkdir_p source 
+            FileUtils.chmod 0777, source
+          end
+          seen_mounts[target] = source
+          mount_conf << " -v #{File.expand_path(source)}:#{target}"
+        end
+      end
+    end
+
+    if infrastructure[:workflow_autoinstall] and infrastructure[:workflow_autoinstall].to_s == 'true' and cmd =~ /rbbt/
+      cmd = "env RBBT_WORKFLOW_AUTOINSTALL=true " + cmd
+    end
+
+    umask = infrastructure[:umask] ? 'umask 000; ' : ''
+    name_conf = options[:name]
+    name_conf = "--name " << name_conf if name_conf
+    name_conf ||= ""
+    cmd_str = "docker run #{name_conf} #{mount_conf} #{user_conf} #{docker_args*" "} #{image} /bin/bash --login -c '#{umask}#{cmd} #{cmd_args*" "}"
+    cmd_str += " --log #{Log.severity} " if cmd =~  /\brbbt$/
+    cmd_str += "'" 
+
+    Log.info "Docker: \n" << cmd_str
+
+    exec(cmd_str) unless options[:dry_run]
+  end
+end

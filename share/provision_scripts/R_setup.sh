837090ecf3fb2fcce2abe7aae23b7071b71f5c07
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2017-11-09 09:49:27 +0100

    nil

--- share/provision_scripts/R_setup.sh
+++ share/provision_scripts/R_setup.sh
@@ -7,10 +7,10 @@ cd /tmp
 
 apt-get remove r-base-core
 
-wget https://cran.r-project.org/src/base/R-3/R-3.3.2.tar.gz
-tar -xvzf R-3.3.2.tar.gz
+wget https://ftp.gwdg.de/pub/misc/cran/src/base/R-3/R-3.4.2.tar.gz -O R.tar.gz
+tar -xvzf R.tar.gz
 
-cd R-3.3.2/
+cd R-*/
 ./configure --prefix=/usr/local --enable-R-shlib
 make && make install
 
